5aca8abffaf71332991f6f0dfafff719a8db6fb2
md.yusuf@tigerit.com <md.yusuf@tigerit.com>
2020-11-04 19:33:37 +0600

    dummy changes

--- src/Generic/GenericClass.java
+++ src/Generic/GenericClass.java
@@ -4,7 +4,9 @@ public class GenericClass  < T1, T2 >{
     public void display (T1 var1, T2 var2){
         System.out.println("Name: " + var1 + " ID: " + var2);
     }
-
+	public void dummy () {
+		
+	}
     public static void main(String[] args) {
         GenericClass<String, Integer> obj = new GenericClass<String, Integer>();
         obj.display("Monica", 1234);
